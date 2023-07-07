83db4f34183e73a876a0a4f70b2a18a2ead471d8
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-07 21:22:19 +0530

    update

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
 
+import clip 
 from ddpo_pytorch.aesthetic_scorer import aesthetic_scoring
 
+# setup reward model
+clip_model, preprocess = clip.load("ViT-L/14", device="cuda")
 aesthetic_model = MLP(768)
-aesthetic_model.load_state_dict(weights)
+aesthetic_model.load_state_dict(load_aesthetic_model_weights())
+aesthetic_model.cuda()
 
 def reward_fn(imgs, device):
     clip_model.to(device)
@@ -12,5 +16,4 @@ def reward_fn(imgs, device):
 
     clip_model.to("cpu")
     aesthetic_model.to("cpu")
-    
     return rewards
