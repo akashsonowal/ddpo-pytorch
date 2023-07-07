c6f24c5e082ae6fb305671978f25d15594a112f6
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-07 22:37:42 +0530

    update

--- ddpo_pytorch/utils.py
+++ ddpo_pytorch/utils.py
@@ -1,4 +1,10 @@
+import math 
 import torch 
+
+def calculate_log_probs(prev_sample, prev_sample_mean, std_dev_t):
+    std_dev_t = torch.clip(std_dev_t, 1e-6)
+    log_probs = -((prev_sample.detach() - prev_sample_mean) ** 2) / (2 * std_dev_t ** 2) - torch.log(std_dev_t) - math.log(math.sqrt(2 * math.pi))
+    return log_probs
 
 @torch.no_grad()
 def sd_sample(prompts, pipe, height, width, guidance_scale, num_inference_steps, eta, device):
@@ -19,19 +25,21 @@ def sd_sample(prompts, pipe, height, width, guidance_scale, num_inference_steps,
         input = scheduler.scale_model_input(input, t) # (8, 4, 64, 64)
 
         # predict the noise residual
-        pred = unet(input, t, encoder_hidden_states=text_embeddings).sample
+        pred = unet(input, t, encoder_hidden_states=text_embeddings).sample # (8, 4, 64, 64) noise
 
         # perform guidance
         pred_uncond, pred_text = pred.chunk(2)
-        pred = pred_uncond + guidance_scale * (pred_text - pred_uncond)
+        pred = pred_uncond + guidance_scale * (pred_text - pred_uncond) # (4, 4, 64, 64)
 
         # compute the "previous" noisy sample mean and variance, and get log probs
         scheduler_output = scheduler.step(pred, t, latents, eta, variance_noise=0)
         t_1 = t - scheduler.config.num_train_timesteps // num_inference_steps
         variance = scheduler._get_variance(t, t_1)
         std_dev_t = eta * variance ** (0.5)
+
         prev_sample_mean = scheduler_output.prev_sample # this is the mean and not full sample since variance is 0
         prev_sample = prev_sample_mean + torch.randn_like(prev_sample_mean) * std_dev_t # get full sample by adding noise
+
         log_probs.append(calculate_log_probs(prev_sample, prev_sample_mean, std_dev_t).mean(dim=tuple(range(1, prev_sample_mean.ndim))))
 
         all_step_preds.append(prev_sample)
