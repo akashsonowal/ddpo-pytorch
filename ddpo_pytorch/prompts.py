e560b3bf55fe79c31a811edeaf9a6cf5054b7e20
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-07 22:45:03 +0530

    update

--- ddpo_pytorch/prompts.py
+++ ddpo_pytorch/prompts.py
@@ -1,4 +1,17 @@
 import random 
+import torch 
+
+class PromptDataset(torch.utils.data.Dataset):
+    def __init__(self, prompt_fn, num):
+        super().__init__()
+        self.prompt_fn = prompt_fn
+        self.num = num
+        
+    def __len__(self): 
+        return self.num
+
+    def __getitem__(self, x): 
+        return self.prompt_fn()
 
 def imagent_animal_prompts():
     animal = random.choice(imagenet_classes[:397])
