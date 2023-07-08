42a3790ac9f6e8c6c8eddbfdcf8599567f7d2c9b
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-08 15:54:37 +0530

    Update prompts.py

--- ddpo_pytorch/prompts.py
+++ ddpo_pytorch/prompts.py
@@ -1,5 +1,21 @@
-import random 
+import random
+import requests
 import torch 
+
+r = requests.get(
+        "https://raw.githubusercontent.com/formigone/tf-imagenet/master/LOC_synset_mapping.txt"
+    )
+with open("LOC_synset_mapping.txt", "wb") as f:
+    f.write(r.content)
+
+synsets = {
+    k: v
+    for k, v in [
+        o.split(",")[0].split(" ", maxsplit=1)
+        for o in Path("LOC_synset_mapping.txt").read_text().splitlines()
+    ]
+}
+imagenet_classes = list(synsets.values())  # total 1000 classes
 
 class PromptDataset(torch.utils.data.Dataset):
     def __init__(self, prompt_fn, num):
@@ -13,7 +29,7 @@ class PromptDataset(torch.utils.data.Dataset):
     def __getitem__(self, x): 
         return self.prompt_fn()
 
-def imagenet_animal_prompts(imagenet_classes):
+def imagenet_animal_prompts():
     animal = random.choice(imagenet_classes[:397])
     prompts = f"{animal}"
     return prompts
