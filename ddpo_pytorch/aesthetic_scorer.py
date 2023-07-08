66217bc3ddb79ff62c081f6189503ce574ea71cb
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-08 15:34:00 +0530

    Update aesthetic_scorer.py

--- ddpo_pytorch/aesthetic_scorer.py
+++ ddpo_pytorch/aesthetic_scorer.py
@@ -4,6 +4,7 @@ import requests
 
 from PIL import Image
 
+
 class MLP(nn.Module):
     def __init__(self, input_size):
         super(MLP).__init__()
@@ -16,14 +17,15 @@ class MLP(nn.Module):
             nn.Linear(128, 64),
             nn.Dropout(0.1),
             nn.Linear(64, 16),
-            nn.Linear(16, 1)
+            nn.Linear(16, 1),
         )
-    
+
     def __repr__(self):
         return """This is the aesthetic model based on https://github.com/christophschuhmann/improved-aesthetic-predictor/blob/fe88a163f4661b4ddabba0751ff645e2e620746e/simple_inference.py."""
-    
+
     def forward(self, x):
         return self.layers(x)
+
 
 def load_aesthetic_model_weights(cache="."):
     weights_fname = "sac+logos+ava1-l14-linearMSE.pth"
@@ -38,28 +40,36 @@ def load_aesthetic_model_weights(cache="."):
 
         with open(loadpath, "wb") as f:
             f.write(r.content)
-    
+
     weights = torch.load(loadpath, map_location=torch.device("cpu"))
-    
+
     return weights
 
-def aesthetic_model_normalize(a, axis=-1, order=2):
-    l2 = np.atleast_1d(np.linalg.norm(a, order, axis)) # calculate l2 norms of each row
-    l2[l2 == 0] = 1 # setting 0 norms as 1
-    return a / np.expand_dims(l2, axis) # transpose
 
-def aesthetic_scoring(imgs, preprocess, clip_model, aesthetic_model_normalize, aesthetic_model):
+def aesthetic_model_normalize(a, axis=-1, order=2):
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))  # calculate l2 norms of each row
+    l2[l2 == 0] = 1  # setting 0 norms as 1
+    return a / np.expand_dims(l2, axis)  # transpose
+
+
+def aesthetic_scoring(
+    imgs, preprocess, clip_model, aesthetic_model_normalize, aesthetic_model
+):
     """
     input imgs: (4, 512, 512, 3) for batch size of 4
     returns: (4, 1)
     """
-    imgs = torch.stack([preprocess(Image.fromarray(img)).cuda() for img in imgs]) # (4, 3, 224, 224)
+    imgs = torch.stack(
+        [preprocess(Image.fromarray(img)).cuda() for img in imgs]
+    )  # (4, 3, 224, 224)
 
     with torch.no_grad():
-        image_features = clip_model.encode(imgs) # (4, 768)
-    
-    im_emb_arr = aesthetic_model_normalize(image_features.cpu().detach().numpy()) # (4, 768)
+        image_features = clip_model.encode(imgs)  # (4, 768)
 
-    prediction = aesthetic_model(torch.from_numpy(im_emb_arr).float().cuda()) # (4, 1)
+    im_emb_arr = aesthetic_model_normalize(
+        image_features.cpu().detach().numpy()
+    )  # (4, 768)
+
+    prediction = aesthetic_model(torch.from_numpy(im_emb_arr).float().cuda())  # (4, 1)
 
     return prediction
