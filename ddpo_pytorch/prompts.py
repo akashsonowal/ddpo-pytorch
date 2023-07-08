53c6c9e92d621b9513f54b537f39eb4289f74b46
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-08 19:05:54 +0530

    update

--- ddpo_pytorch/prompts.py
+++ ddpo_pytorch/prompts.py
@@ -4,22 +4,26 @@ import requests
 from pathlib import Path
 import torch
 
+
 def get_prompts():
     if not os.path.exists("LOC_synset_mapping.txt"):
-        r = requests.get("https://raw.githubusercontent.com/formigone/tf-imagenet/master/LOC_synset_mapping.txt")
-        
+        r = requests.get(
+            "https://raw.githubusercontent.com/formigone/tf-imagenet/master/LOC_synset_mapping.txt"
+        )
+
         with open("LOC_synset_mapping.txt", "wb") as f:
             f.write(r.content)
-    
+
     synsets = {
-                k: v
-                for k, v in [
-                    o.split(",")[0].split(" ", maxsplit=1)
-                    for o in Path("LOC_synset_mapping.txt").read_text().splitlines()
-                ]
-            }
+        k: v
+        for k, v in [
+            o.split(",")[0].split(" ", maxsplit=1)
+            for o in Path("LOC_synset_mapping.txt").read_text().splitlines()
+        ]
+    }
     imagenet_classes = list(synsets.values())  # total 1000 classes
     return imagenet_classes
+
 
 class PromptDataset(torch.utils.data.Dataset):
     def __init__(self, prompt_fn, num):
